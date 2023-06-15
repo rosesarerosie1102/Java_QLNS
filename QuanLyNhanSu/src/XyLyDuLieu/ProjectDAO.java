c44cd3c42aac84f6b0cfff035e4f6f4e857aca6f
Dajo <81942388+rosesarerosie1102@users.noreply.github.com>
2023-06-16 01:30:26 +0700

    Update ProjectDAO.java

--- QuanLyNhanSu/src/XyLyDuLieu/ProjectDAO.java
+++ QuanLyNhanSu/src/XyLyDuLieu/ProjectDAO.java
@@ -12,10 +12,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author thanhbm
- */
+
 public class ProjectDAO {
     /*selectAllProject method select all record from Projects table*/
 
